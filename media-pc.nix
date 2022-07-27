375c13d8efebf13356eded78c99627b5dfec707f
Julien DOCHE <j.doche@criteo.com>
2022-07-27 23:34:29 +0200

    scale
    Signed-off-by: Julien DOCHE <j.doche@criteo.com>

--- media-pc.nix
+++ media-pc.nix
@@ -7,4 +7,14 @@
     libretro.snes9x
     libretro.beetle-psx-hw
   ];
+
+  # bigger tty fonts
+  console.font =
+    "${pkgs.terminus_font}/share/consolefonts/ter-u28n.psf.gz";
+  services.xserver.dpi = 180;
+  environment.variables = {
+    GDK_SCALE = "2";
+    GDK_DPI_SCALE = "0.5";
+    _JAVA_OPTIONS = "-Dsun.java2d.uiScale=2";
+  };
 }
